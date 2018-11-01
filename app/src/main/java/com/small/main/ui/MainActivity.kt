86aefb528951d07292cb7384b648770e4e2b3b9c
Adik Widiasmono <adik.widiasmono@gmail.com>
2018-11-02 06:24:33 +0700

    Add matches fragement

--- app/src/main/java/com/small/main/ui/MainActivity.kt
+++ app/src/main/java/com/small/main/ui/MainActivity.kt
@@ -5,11 +5,12 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
 import android.util.Log
 import android.view.Menu
-import android.widget.SearchView
 import com.small.main.R
 import com.small.main.ui.favoritematch.FavoriteMatchFragment
+import com.small.main.ui.matches.MatchesFragment
 import com.small.main.ui.previousmatch.PrevMatchFragment
 import com.small.main.ui.teams.TeamsFragment
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,14 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toolbar.title = "Football Match Schedule"
-        setSupportActionBar(toolbar)
-
         bnv_football_match.setOnNavigationItemSelectedListener { item ->
             fragmentState = item.itemId
             when (item.itemId) {
                 R.id.it_matches -> {
-                    loadFragment(savedInstanceState, PrevMatchFragment(), getString(R.string.matches))
+                    loadFragment(savedInstanceState, MatchesFragment(), getString(R.string.matches))
                 }
                 R.id.it_teams -> {
                     loadFragment(savedInstanceState, TeamsFragment(), getString(R.string.teams))
@@ -49,33 +47,6 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.fl_football_match, activeFragement, activeTitle)
                     .commit()
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.dashboard_menu, menu)
-        val searchItem = menu?.findItem(R.id.it_search)
-        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        var searchView: SearchView? = null
-
-        searchItem.let {
-            searchView = it?.actionView as SearchView
-        }
-        searchView.let {
-            it?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
-            it?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextChange(query: String): Boolean {
-                    Log.e("onQueryTextChange", "->" + query)
-                    return false
-                }
-
-                override fun onQueryTextSubmit(query: String): Boolean {
-                    Log.e("onQueryTextChange", "->" + query)
-                    return false
-                }
-            })
-        }
-
-        return super.onCreateOptionsMenu(menu)
     }
 
 }
---
+++ app/src/main/java/com/small/main/ui/matches/MatchesFragment.kt
@@ -0,0 +1,95 @@
+package com.small.main.ui.matches
+
+import android.app.SearchManager
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
+import android.util.Log
+import android.view.*
+import com.small.main.R
+import com.small.main.ui.nextmatch.NextMatchFragment
+import com.small.main.ui.previousmatch.PrevMatchFragment
+import kotlinx.android.synthetic.main.fragment_matches.*
+import kotlinx.android.synthetic.main.fragment_matches.view.*
+
+class MatchesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_matches, container, false)
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.toolbar_matches.title = "Football Match Schedule"
+        val act = activity as AppCompatActivity
+        act.setSupportActionBar(toolbar_matches)
+
+        setupViewPager(view.vp_matches)
+        view.tl_matches.setupWithViewPager(view.vp_matches)
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(activity?.supportFragmentManager)
+        adapter.addFragment(PrevMatchFragment(), getString(R.string.prev_match))
+        adapter.addFragment(NextMatchFragment(), getString(R.string.next_match))
+        viewPager.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, menuInflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, menuInflater)
+
+        menuInflater?.inflate(R.menu.dashboard_menu, menu)
+        val searchItem = menu?.findItem(R.id.it_search)
+        val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        var searchView: SearchView? = null
+
+        searchItem.let {
+            searchView = it?.actionView as SearchView
+        }
+        searchView.let {
+            it?.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+            it?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(query: String): Boolean {
+                    Log.e("onQueryTextChange", "->" + query)
+                    return false
+                }
+
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    Log.e("onQueryTextSubmit", "->" + query)
+                    return false
+                }
+            })
+        }
+    }
+
+    class ViewPagerAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+        private val listFragment: MutableList<Fragment> = mutableListOf()
+        private val listFragmentTitle: MutableList<String> = mutableListOf()
+
+        override fun getItem(position: Int): Fragment {
+            return listFragment.get(position)
+        }
+
+        override fun getCount(): Int {
+            return listFragment.size
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return listFragmentTitle.get(position)
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            listFragment.add(fragment)
+            listFragmentTitle.add(title)
+        }
+    }
+
+}
